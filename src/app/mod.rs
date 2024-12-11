56d5dd16ae568b3324ecb61e81d0c5e23f08f151
Yogi Astawan <yogi.astawan@gmail.com>
2024-12-11 18:20:58 +0800

    create 2 triangle using vertex buffer

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -52,6 +52,18 @@ const VERTICES: &[Vertex] = &[
         position: [0.5, -0.5, 0.0],
         color: [0.0, 0.0, 1.0],
     },
+    Vertex {
+        position: [0.6, 0.6, 0.0],
+        color: [1.0, 1.0, 0.0],
+    },
+    Vertex {
+        position: [-0.4, 0.6, 0.0],
+        color: [0.0, 1.0, 1.0],
+    },
+    Vertex {
+        position: [0.6, -0.4, 0.0],
+        color: [1.0, 0.0, 1.0],
+    },
 ];
 
 pub struct XApp<'l> {
